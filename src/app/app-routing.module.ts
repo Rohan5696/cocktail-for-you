3e585938fd2cd71d0e69553728b9fc7c0500d95b
Rohan 1. Mishra (EXT-Nokia) <rohan.1.mishra.ext@nokia.com>
2023-08-26 19:53:48 +0530

    init-code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { CocktailMenuComponent } from './page/cocktail-menu/cocktail-menu.component';
+import { IngredientComponent } from './page/ingredient/ingredient.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DashboardComponent
+}, {
+  path: 'cocktail/:id',
+  component: CocktailMenuComponent
+}, {
+  path: 'ingredient/:id',
+  component: IngredientComponent
+}, {
+  path: '**',
+  redirectTo: ''
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
